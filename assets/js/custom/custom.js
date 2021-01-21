8ca268e7df211857d3f6a85ddc7d0afed3514e4f
zakaria ben salek <zakariaazaraf@gmail.com>
2021-01-21 19:53:52 +0100

    finish the home and the header responsiveness features

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -4,6 +4,29 @@ const menu = document.querySelector('header nav .nav-menu')
 const showMenu = document.querySelector('header .show-menu')
 const closeMenu = document.querySelector('header .close-menu')
 
+window.addEventListener('load', (e)=>{
+    if(window.innerWidth >= 992){
+        menu.classList.remove('sided-menu')
+        closeMenu.style.display = 'none'
+        showMenu.style.display = 'none'
+        menu.style.transform = "translateX("+ 0 +"%)";
+        
+    }else{  
+        menu.classList.add('sided-menu')
+        closeMenu.style.display = 'inline-block'
+        showMenu.style.display = 'inline-block'
+        menu.style.transform = "translateX("+ 100 +"%)";
+        
+    }
+
+    if(window.pageYOffset > 100){
+        header.classList.add('bg-scroll')
+    }else{  
+        header.classList.remove('bg-scroll')
+    }
+    
+})
+
 
 // SHOW THE MENU 
 showMenu.addEventListener('click', ()=>{
@@ -21,4 +44,20 @@ document.addEventListener('scroll', (e) => {
     }else{  
         header.classList.remove('bg-scroll')
     }
+})
+
+window.addEventListener('resize', (e) => {
+    if(e.target.innerWidth >= 992){
+        menu.classList.remove('sided-menu')
+        closeMenu.style.display = 'none'
+        showMenu.style.display = 'none'
+        menu.style.transform = "translateX("+ 0 +"%)";
+        
+    }else{  
+        menu.classList.add('sided-menu')
+        closeMenu.style.display = 'inline-block'
+        showMenu.style.display = 'inline-block'
+        menu.style.transform = "translateX("+ 100 +"%)";
+        
+    }  
 })
