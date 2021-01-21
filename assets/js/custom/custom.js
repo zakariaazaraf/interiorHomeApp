d19c49d5489ca6a202aac57c40ee283649f3a5de
zakaria ben salek <zakariaazaraf@gmail.com>
2021-01-21 18:44:22 +0100

    add the sided menu and the their features

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -1,1 +1,24 @@
 /* Create Your Javascript Code */
+const header = document.querySelector('header')
+const menu = document.querySelector('header nav .nav-menu')
+const showMenu = document.querySelector('header .show-menu')
+const closeMenu = document.querySelector('header .close-menu')
+
+
+// SHOW THE MENU 
+showMenu.addEventListener('click', ()=>{
+    menu.style.transform = "translateX("+ 0 +"%)";
+})
+
+// HIDE THE MENU
+closeMenu.addEventListener('click', ()=>{
+    menu.style.transform = "translateX("+ 100 +"%)";
+})
+
+document.addEventListener('scroll', (e) => {
+    if(window.pageYOffset > 100){
+        header.classList.add('bg-scroll')
+    }else{  
+        header.classList.remove('bg-scroll')
+    }
+})
